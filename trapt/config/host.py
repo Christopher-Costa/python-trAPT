8c925d530aef5c548e8e2278426bc66e9e7f7d71
Christopher Costa <Christopher.costa@gmail.com>
2018-09-02 01:50:34 +0000

    validate_config called in base class;  improve documentation

--- trapt/config/host.py
+++ trapt/config/host.py
@@ -1,5 +1,6 @@
 import config.json
 import tools.ip
+import tools.port
 import sys
 
 class Host(config.json.Json):
@@ -11,16 +12,49 @@ class Host(config.json.Json):
         config.json.Json.__init__(self, config_file)
         print("Loading complete...")
 
-        self.validate_host_config()
+    def validate_config(self):
+        """
+        Verify that the supplied host configuration contains
+        reasonable and valid information, to the extent
+        possible.
 
-    def validate_host_config(self):
-        for host_range in self.config:
-            if (not tools.ip.is_ipv4_address(host_range) and not tools.ip.is_ipv4_range(host_range)):
-                print ('"' + host_range + '" is not a valid IPv4 address or IPv4 range')
-                sys.exit()
+        Host configurations are expected in the following format:
 
-#            if not self.config[host_range]['gateway'] in self.router_config.config:
-#                raise Exception('host range "' + host_range + '" references non-existant gateway')
-            
-            for host in tools.ip.ipv4_address_list(host_range):
-                pass
+            {
+                "<address or address range> : {
+                    "gateway": "XXX.XXX.XXX.XXX",
+                    "default_state": "<blocked|open|reset>",
+                    "ports" : {
+                        "<port or port range> : { "state" : <blocked|open|reset> },
+                        ...
+                    }
+                },
+                ...
+            }
+
+        If problems are detected, Print an informative message 
+        and exit the program.
+        """
+
+        errors = []
+        for hosts in self.config:
+            if (not tools.ip.is_ipv4_address(hosts) 
+                    and not tools.ip.is_ipv4_range(hosts)):
+                errors.append('host "{0}" is not a valid IPv4 address or IPv4 range.'.format(hosts))
+
+            gateway = self.config[hosts]['gateway']    
+            if not tools.ip.is_ipv4_address(gateway):
+                errors.append('gateway "{0}" is not a valid IPv4 address.'.format(gateway))
+
+            if not gateway in self.router_config.config:
+                errors.append('gateway "{0}" does not exist in router configuration.'.format(gateway))
+           
+            for ports in self.config[hosts]['ports']:
+                if not tools.port.is_port(ports) and not tools.port.is_port_range(ports):
+                    errors.append('"{0}" is not a valid port or port range.'.format(ports))
+
+        if errors:
+            print('Error validating host config:')
+            for error in errors:
+                print('  ' + error)
+            sys.exit()
