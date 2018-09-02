bc845274bbd2d33eda20da41386763e4e0fde21e
Christopher Costa <Christopher.costa@gmail.com>
2018-09-02 01:46:57 +0000

    Handle exceptions when loading config; Add config validation

--- trapt/config/json.py
+++ trapt/config/json.py
@@ -1,11 +1,20 @@
 import json
+import sys
 
 class Json(): 
 
     def __init__(self, config_file):
         self.config_file = config_file
         self.parse_config()
+        self.validate_config()
 
     def parse_config(self):
-        with open(self.config_file) as config_file:
-            self.config = json.load(config_file)
+        try:
+            with open(self.config_file) as config_file:
+                self.config = json.load(config_file)
+        except ValueError as error:
+            print("Unable to load configuration from {0}: {1}".format(self.config_file, error))
+            sys.exit()  
+
+    def validate_config(self):
+        return True
