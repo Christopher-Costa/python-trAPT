271c6ac17da5d90be04a7106c4e6e21a472f11ae
Christopher Costa <Christopher.costa@gmail.com>
2018-09-02 17:35:28 +0000

    Complete the host interfaces table during init.

--- trapt/config/host.py
+++ trapt/config/host.py
@@ -12,6 +12,10 @@ class Host(config.json.Json):
         config.json.Json.__init__(self, trapt, trapt.arguments.hosts)
         self.trapt.logger['app'].logger.info("Loading complete...")
 
+        self.trapt.logger['app'].logger.info("Building host interface table...")
+        self.build_interface_table()
+        self.trapt.logger['app'].logger.info("Complete")
+
     def validate_config(self):
         """
         Verify that the supplied host configuration contains
@@ -40,20 +44,52 @@ class Host(config.json.Json):
         for hosts in self.config:
             if (not tools.ip.is_ipv4_address(hosts) 
                     and not tools.ip.is_ipv4_range(hosts)):
-                errors.append('host "{0}" is not a valid IPv4 address or IPv4 range.'.format(hosts))
+                error_message = 'host "{0}" is not a valid IPv4 address or IPv4 range.'
+                errors.append(error_message.format(hosts))
 
             gateway = self.config[hosts]['gateway']    
             if not tools.ip.is_ipv4_address(gateway):
-                errors.append('gateway "{0}" is not a valid IPv4 address.'.format(gateway))
+                error_message = 'gateway "{0}" is not a valid IPv4 address.'
+                errors.append(error_message.format(gateway))
 
-            if not gateway in self.trapt.config['router'].interfaces:
-                errors.append('gateway "{0}" does not exist in router configuration.'.format(gateway))
+            if (not gateway in self.trapt.config['router'].interfaces and
+                    not gateway == '0.0.0.0'):
+                error_message = 'gateway "{0}" does not exist in router configuration.'
+                errors.append(error_message.format(gateway))
            
             for ports in self.config[hosts]['ports']:
                 if not tools.port.is_port(ports) and not tools.port.is_port_range(ports):
-                    errors.append('"{0}" is not a valid port or port range.'.format(ports))
+                    error_message = '"{0}" is not a valid port or port range.'
+                    errors.append(error_message.format(ports))
 
         if errors:
             for error in errors:
-                self.trapt.logger['app'].logger.error('Error in host config: {0}'.format(error))
+                error_message = 'Error in host config: {0}'
+                self.trapt.logger['app'].logger.error(error_message.format(error))
             sys.exit()
+
+    def build_interface_table(self):
+        self.interfaces = {}
+        for hosts in self.config:
+            gateway = self.config[hosts]['gateway']
+            if gateway in self.trapt.config['router'].interfaces:
+                network = self.trapt.config['router'].interfaces[gateway]['network']
+                latency = self.trapt.config['router'].route_table[network]['latency']
+                external = self.trapt.config['router'].route_table[network]['external']
+
+            elif gateway == '0.0.0.0':
+                latency = 0
+                external = 1
+
+            else:
+                error_message = 'gateway "{0}" does not exist in router configuration.'
+                self.trapt.logger['app'].logger.error(error_message.format(error))
+                sys.exit()
+
+            for host in tools.ip.ipv4_address_list(hosts):
+                self.interfaces[host] = {}
+                self.interfaces[host]['latency'] = latency
+                self.interfaces[host]['external'] = external
+
+    def is_external(self, address):
+        return self.interfaces[address]['external']
