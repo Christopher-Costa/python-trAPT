0fa40ceb360484ed029a8c4d110b96df694392fd
Christopher Costa <Christopher.costa@gmail.com>
2018-09-02 13:32:28 +0000

    Frame handler processes ARP and ICMP

--- trapt/core/receiver.py
+++ trapt/core/receiver.py
@@ -1,4 +1,6 @@
-from scapy.all import sniff
+import scapy.all
+import handler.arp
+import handler.icmp
 import configparser
 import time
 
@@ -9,18 +11,22 @@ class Receiver:
         self.interface = self.trapt.config['main'].settings['general']['interface']
         self.filter = self.trapt.config['main'].settings['general']['filter']
 
-    def packet_handler(self, packet):
+    def frame_handler(self, frame):
         """ 
-        Parse the packets and pass to the appropriate protocol handler
+        Function to parse the sniffed frames and pass to the appropriate 
+        protocol handler.
         """
-        
-        #print(self.interface + ": " + packet.summary())
+
+        if frame.haslayer(scapy.all.ARP):
+            handler.arp.Arp(frame, self.trapt)
+        elif frame.haslayer(scapy.all.ICMP):
+            handler.icmp.Icmp(frame, self.trapt)
 
     def start(self):
         """
-        Begin the capture process on the configured network interface.  Pass any packets
-        matching the configured filter to the handler function.
+        Process to begin the capture process on the configured network interface
+        and pass any frames matching the configured filter to the handler function.
         """
 
         self.trapt.logger['app'].logger.info("Starting capture on interface {0}".format(self.interface))
-        sniff(prn = self.packet_handler, store=0, filter=self.filter, iface=self.interface)
+        scapy.all.sniff(prn = self.frame_handler, store=0, filter=self.filter, iface=self.interface)
