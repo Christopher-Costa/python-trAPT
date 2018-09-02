4b277325abb126eef0309de80ceb830298092265
Christopher Costa <Christopher.costa@gmail.com>
2018-09-02 19:52:02 +0000

    add helpful function for port list.

--- trapt/tools/port.py
+++ trapt/tools/port.py
@@ -25,3 +25,21 @@ def is_port_range(port_range):
             if (int(ports[0]) < int(ports[1]) ):
                 return True
     return False
+
+def port_list(port_range):
+    """
+    Helper function to return a list of ports. The function accepts
+    strings containing a valid port or port range.
+    """
+
+    if is_port(port_range):
+        return [port_range]
+
+    elif is_port_range(port_range):
+        start_port, end_port = port_range.split('-')
+
+        port_list = []
+        for port in range(int(start_port), int(end_port)+1):
+            port_list.append(str(port))
+        return port_list
+
