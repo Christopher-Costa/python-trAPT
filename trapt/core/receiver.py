dae0e11cba721b5f650e094e1cfc47dd01f4f204
Christopher Costa <Christopher.costa@gmail.com>
2018-09-03 02:00:27 +0000

    handle received TCP traffic.

--- trapt/core/receiver.py
+++ trapt/core/receiver.py
@@ -1,6 +1,7 @@
 import scapy.all
 import handler.arp
 import handler.icmp
+import handler.tcp
 import configparser
 import time
 
@@ -19,8 +20,17 @@ class Receiver:
 
         if frame.haslayer(scapy.all.ARP):
             handler.arp.Arp(frame, self.trapt)
+
         elif frame.haslayer(scapy.all.ICMP):
             handler.icmp.Icmp(frame, self.trapt)
+
+        elif frame.haslayer(scapy.all.TCP):
+            if handler.tcp.connection_exists(frame):
+                connection_key = handler.tcp.connection_key(frame)
+                existing_connection = handler.tcp.Tcp.connection_table[connection_key]
+                existing_connection.handle(frame)
+            else:
+                handler.tcp.Tcp(frame, self.trapt)
 
     def start(self):
         """
